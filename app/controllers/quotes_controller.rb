97218c07bbdd21e611e60d0887c571bfdced9bb8
pamela Adams <pwadams@me.com>
2015-02-18 14:29:38 -0700

    debugging exercise

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -4,4 +4,28 @@ class QuotesController < ApplicationController
     @quotes = Quote.all
   end
 
+  def new
+    @quote = Quote.new
+  end
+
+  def create
+    @quote = Quote.new(params.require).permit(:text, :author, :create_at, :updated_at)
+    @quote.save
+    redirect_to quotes_path
+  end
+
+  def show
+    @quote = Quote.find(params[:id])
+  end
+
+  def edit
+    @quote = Quote.find(params[:id])
+  end
+
+  def update
+    @quote = Quote.find(params[:id])
+    @quote.update(params.require).permit(:text, :author, :create_at, :updated_at)
+  end
+
+  
 end
