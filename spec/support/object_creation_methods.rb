3d5ca294ccbbcf646f63ae1b516f262256b78b1c
Ashley Nguyen <a.nguyen020@gmail.com>
2016-02-29 14:06:13 -0800

    add more tests

--- spec/support/object_creation_methods.rb
+++ spec/support/object_creation_methods.rb
@@ -6,3 +6,13 @@ def create_user(overrides = {})
     password_confirmation: 'password'
   }.merge(overrides))
 end
+
+def login
+  create_user email: "user@example.com"
+
+  visit root_path
+  click_on "Login"
+  fill_in "Email", with: "user@example.com"
+  fill_in "Password", with: "password"
+  click_on "Login"
+end
