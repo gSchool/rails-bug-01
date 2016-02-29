460314fc4533243253479c0f21aa92cf6944997d
Jon Harlem <jtharlem@gmail.com>
2016-02-29 14:00:59 -0800

    added test for relative date 2 days ago

--- spec/features/date_spec.rb
+++ spec/features/date_spec.rb
@@ -17,7 +17,7 @@ feature 'created_at' do
   end
 
 	scenario 'Users can view less than a minute ago for quotes without created_at' do
-		create_user email: "user@example.com"
+	create_user email: "user@example.com"
     Quote.create!(text: %Q{Something pithy})
     Quote.create!(text: %Q{Something cool})
 
@@ -29,4 +29,19 @@ feature 'created_at' do
 
     expect(page).to have_content("less than a minute ago")
   end
+
+    scenario 'Users can view less than a minute ago for quotes without created_at' do
+    create_user email: "user@example.com"
+    Quote.create!(text: %Q{Something pithy}, author: %Q{Buddy Hackett}, created_at: (Date.today-1), updated_at: (Date.today-1))
+
+    visit root_path
+    click_on "Login"
+    fill_in "Email", with: "user@example.com"
+    fill_in "Password", with: "password"
+    click_on "Login"
+
+    expect(page).to have_content("2 days ago")
+  end
+
+
 end
