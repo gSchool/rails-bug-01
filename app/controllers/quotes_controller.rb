89369b8e82cbee8d37b2a1582b5ea5ba8859dbf4
sirinapha88 <sirinapha.dube@gmail.com>
2016-02-29 12:22:21 -0800

    made change index.html.erb

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -4,10 +4,4 @@ class QuotesController < ApplicationController
     @quotes = Quote.all
   end
 
-  def create
-    @quote = Quote.new(params.require).permit(:text, :author, :create_at, :updated_at)
-    @quote.save
-    redirect_to quotes_path
-  end
-
 end
