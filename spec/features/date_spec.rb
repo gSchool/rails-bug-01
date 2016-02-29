536d00ca9b407f9ac47cd1aa8617cc8f2fb66548
Jon Harlem <jtharlem@gmail.com>
2016-02-29 13:32:38 -0800

    added test for rendering div.created

--- spec/features/date_spec.rb
+++ spec/features/date_spec.rb
@@ -2,6 +2,19 @@ require 'rails_helper'
 require 'capybara/rails'
 
 feature 'created_at' do
+    scenario 'div.created will render' do
+    create_user email: "user@example.com"
+    Quote.create!(text: %Q{Something pithy})
+    Quote.create!(text: %Q{Something cool})
+
+    visit root_path
+    click_on "Login"
+    fill_in "Email", with: "user@example.com"
+    fill_in "Password", with: "password"
+    click_on "Login"
+
+    expect(page).to have_selector("div.created", text: /.+/)
+  end
 
 	scenario 'Users can view less than a minute ago for quotes without created_at' do
 		create_user email: "user@example.com"
