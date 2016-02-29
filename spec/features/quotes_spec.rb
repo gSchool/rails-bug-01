3a1564d36f2515dd8e0856eee21dcb629a83442e
Lissa Walzer <lissawalzer@gmail.com>
2016-02-29 14:51:04 -0800

    Added a test for displaying the date

--- spec/features/quotes_spec.rb
+++ spec/features/quotes_spec.rb
@@ -18,4 +18,17 @@ feature 'Auth' do
     expect(page).to have_content("Something cool")
   end
 
+  scenario 'Users can see a quote creation date' do
+    create_user email: "user@example.com"
+    Quote.create!(text: %Q{Something pithy}, author: %Q{Someone}, created_at: (Date.today-6))
+
+    visit root_path
+    click_on "Login"
+    fill_in "Email", with: "user@example.com"
+    fill_in "Password", with: "password"
+    click_on "Login"
+
+    expect(page).to have_content("7 days ago")
+  end
+
 end
