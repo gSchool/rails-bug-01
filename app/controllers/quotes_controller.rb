adbeec01e518965d39a8a71137fc76c385939be7
sirinapha88 <sirinapha.dube@gmail.com>
2016-02-29 11:44:49 -0800

    add created function & made changed index.html.erb

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -4,4 +4,10 @@ class QuotesController < ApplicationController
     @quotes = Quote.all
   end
 
+  def create
+    @quote = Quote.new(params.require).permit(:text, :author, :create_at, :updated_at)
+    @quote.save
+    redirect_to quotes_path
+  end
+
 end
