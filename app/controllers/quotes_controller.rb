e6e681fbd2d4f09aea2f5c5a29a2504a66f7d681
Ashley Nguyen <a.nguyen020@gmail.com>
2016-02-29 12:10:05 -0800

    use Time.now if quote.created_at is null

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,15 +1,7 @@
-require 'time_ago_in_words'
-
 class QuotesController < ApplicationController
 
   def index
     @quotes = Quote.all
   end
-
-  def time_ago_in_words(time)
-    (Time.now - time.to_i).ago_in_words
-  end
-
-  helper_method :time_ago_in_words
 
 end
