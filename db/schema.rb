88569e330c60bba6e702d20b2e5fed5193e809d2
Mauricio Andrades <mod.and.op@gmail.com>
2016-03-01 12:47:37 -0800

    cleaned up .gitignore(added local entries), added null validatino to schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -17,10 +17,10 @@ ActiveRecord::Schema.define(version: 20140712034016) do
   enable_extension "plpgsql"
 
   create_table "quotes", force: true do |t|
-    t.text     "text"
-    t.string   "author"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.text     "text",       null: false
+    t.string   "author",     null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: true do |t|
