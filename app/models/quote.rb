07d11c26f5a260e4b967e2b0a83280e3cb14b1cc
gregsim92 <gregsim92@gmail.com>
2016-02-29 16:37:51 -0800

    updated quotes validation, no null values

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,3 +1,8 @@
 class Quote < ActiveRecord::Base
+	is_complete
 
+	validates :text, presence:true
+	validates :author, presence:true
+	validates :created_at, presence:true
+	validates :updated_at, presence:true
 end
