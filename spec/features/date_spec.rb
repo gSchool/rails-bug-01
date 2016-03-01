5f8e29c08a28d6f7788fa25a25873eec39bd1de9
Jon Harlem <jtharlem@gmail.com>
2016-02-29 16:17:32 -0800

    added validation and modified test appropriately

--- spec/features/date_spec.rb
+++ spec/features/date_spec.rb
@@ -4,8 +4,8 @@ require 'capybara/rails'
 feature 'created_at' do
     scenario 'div.created will render' do
     create_user email: "user@example.com"
-    Quote.create!(text: %Q{Something pithy})
-    Quote.create!(text: %Q{Something cool})
+    Quote.create!(text: %Q{Something pithy}, author: %Q{Buddy Hackett}, created_at: (Date.today-1), updated_at: (Date.today-1))
+    Quote.create!(text: %Q{Something cool}, author: %Q{Buddy Hackett}, created_at: (Date.today-1), updated_at: (Date.today-1))
 
     visit root_path
     click_on "Login"
@@ -16,21 +16,7 @@ feature 'created_at' do
     expect(page).to have_selector("div.created", text: /.+/)
   end
 
-	scenario 'Users can view less than a minute ago for quotes without created_at' do
-	create_user email: "user@example.com"
-    Quote.create!(text: %Q{Something pithy})
-    Quote.create!(text: %Q{Something cool})
-
-    visit root_path
-    click_on "Login"
-    fill_in "Email", with: "user@example.com"
-    fill_in "Password", with: "password"
-    click_on "Login"
-
-    expect(page).to have_content("less than a minute ago")
-  end
-
-    scenario 'Users can view less than a minute ago for quotes without created_at' do
+    scenario 'Users can view relative timestamp' do
     create_user email: "user@example.com"
     Quote.create!(text: %Q{Something pithy}, author: %Q{Buddy Hackett}, created_at: (Date.today-1), updated_at: (Date.today-1))
 
