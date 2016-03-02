043e5e6ae3bc918e762cb8474b07e3eca592e27f
Jon Tejada <jon@jontejada.com>
2016-03-02 10:50:49 -0800

    added tests to the spec dir

--- spec/features/quotes_spec.rb
+++ spec/features/quotes_spec.rb
@@ -4,18 +4,17 @@ require 'capybara/rails'
 feature 'Auth' do
 
   scenario 'Users can view quotes' do
-    create_user email: "user@example.com"
+    # create_user email: "user@example.com"
     Quote.create!(text: %Q{Something pithy})
     Quote.create!(text: %Q{Something cool})
-
-    visit root_path
-    click_on "Login"
-    fill_in "Email", with: "user@example.com"
-    fill_in "Password", with: "password"
-    click_on "Login"
-
+    login
     expect(page).to have_content("Something pithy")
     expect(page).to have_content("Something cool")
+  end
+  scenario 'quotes will be created_at now()' do
+    Quote.create!(text: %Q{Something cool})
+    login
+    expect(find('div.created')).to have_content(/less than a minute/)
   end
 
 end
